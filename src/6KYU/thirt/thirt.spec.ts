bfb60bde43dd7a9b9ed2899ce7f160b483edc95f
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2020-06-07 16:58:20 +0200

    kata thirt solved

--- src/6KYU/thirt/thirt.spec.ts
+++ src/6KYU/thirt/thirt.spec.ts
@@ -2,5 +2,5 @@ import { thirt } from './thirt';
 
 test('Basic test', () => {
     expect(thirt(1234567)).toStrictEqual(87);
-    // expect(thirt(8529)).toStrictEqual(79);
+    expect(thirt(8529)).toStrictEqual(79);
 });
