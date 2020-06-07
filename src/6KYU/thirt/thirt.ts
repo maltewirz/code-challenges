c65e34c100ddc8aca7241a06ccd1ffc342087360
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2020-06-07 17:03:02 +0200

    added alternative solution to thirt

--- src/6KYU/thirt/thirt.ts
+++ src/6KYU/thirt/thirt.ts
@@ -19,6 +19,14 @@ export function thirt(n: number): number {
     }
 }
 
+// alternative II
+
+// export function thirt(n: number): number {
+//     let remainders: number[] = [1, 10, 9, 12, 3, 4];
+//     let result = n.toString().split('').reverse().map( (c,i) => parseInt(c)*remainders[i%6]).reduce( (p,c) => p += c);
+//     return n == result ? result : thirt(result)
+//   }
+
 
 ////////////////////////////////////////////
 // steps
