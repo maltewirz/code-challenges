fb1ca45104cbebc6285f9ac01afa6baf53379ead
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2020-06-18 09:29:00 +0200

    1 Kata

---
+++ src/6KYU/playPass/playPass.spec.ts
@@ -0,0 +1,8 @@
+import { playPass } from './playPass';
+
+test('Basic test', () => {
+    // expect(playPass('I LOVE YOU!!!', 1)).toStrictEqual('!!!vPz fWpM J');
+    // expect(playPass('2015', 1)).toStrictEqual('4897');
+    // expect(playPass('BORN', 1)).toStrictEqual('oSpC');
+    // expect(playPass('BORN IN 2015!', 1)).toStrictEqual('!4897 Oj oSpC');
+})
