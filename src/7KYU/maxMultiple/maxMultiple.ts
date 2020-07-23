a34f5a38c311c3f8fee1a9908e2320eb1f5d116a
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2020-07-23 06:56:03 +0200

    refactor

--- src/7KYU/maxMultiple/maxMultiple.ts
+++ src/7KYU/maxMultiple/maxMultiple.ts
@@ -1,9 +1,12 @@
 // https://www.codewars.com/kata/5aba780a6a176b029800041c/
 
-export function maxMultiple(divisior: number, bound: number) {
+export function maxMultiple(divisor: number, bound: number) {
     for (let i = bound; i > 0; i --) {
-        if (i % divisior === 0) {
+        if (i % divisor === 0) {
             return i;
         }        
     }
 }
+
+// alternative 
+// return bound - bound % divisor;
