9dd5bd1103ab4335afc0db8f3c8cda3bd03c4d37
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2020-07-31 13:58:10 +0200

    1 Kata 5KYU josephusSurvivor

---
+++ src/5KYU/josephusSurvivor/josephusSurvivor.spec.ts
@@ -0,0 +1,6 @@
+import { josephusSurvivor } from './josephusSurvivor';
+
+test('Basic test', () => {
+    // expect(josephusSurvivor(7, 3)).toStrictEqual(4);
+    expect(josephusSurvivor(11, 19)).toStrictEqual(10);
+})
