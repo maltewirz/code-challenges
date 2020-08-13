3312c0324aaf21d7ad5e58559ce636744eeb2576
Malte Wirz <mail@maltewirz.com>
2020-08-13 09:39:33 +0200

    rename recursive solution

--- src/5KYU/chooseBestSum*/chooseBestSum.spec.ts
+++ src/5KYU/chooseBestSum*/chooseBestSum.spec.ts
@@ -2,4 +2,5 @@ import { chooseBestSum } from './chooseBestSum';
 
 test('Basic test', () => {
     expect(chooseBestSum(174, 3, [50, 55, 57, 58, 60])).toStrictEqual(173);
+    // expect(chooseBestSum(174, 2, [1, 2, 3])).toStrictEqual(5);
 })
