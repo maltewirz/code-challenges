ae035dbb9db3ee89a45f0a839b3adbc10ecfc7bc
Malte Wirz <mail@maltewirz.com>
2020-08-24 13:54:35 +0200

    formatting frequencycounter

--- src/solving-patterns/frequencyCounter.spec.ts
+++ src/solving-patterns/frequencyCounter.spec.ts
@@ -1,14 +1,19 @@
-import { sameOn, sameOn2, anagram, anagramSingleCounter } from './frequencyCounter';
+import {
+    sameOn,
+    sameOn2,
+    anagram,
+    anagramSingleCounter,
+} from './frequencyCounter';
 
 test('sameOn test', () => {
-    expect(sameOn([2,2], [4,4])).toStrictEqual(true);
-    expect(sameOn([1,2,3,2], [4,1,9,4])).toStrictEqual(true);
-    expect(sameOn([1,2,3,3], [4,1,9,4])).toStrictEqual(false);
-})
+    expect(sameOn([2, 2], [4, 4])).toStrictEqual(true);
+    expect(sameOn([1, 2, 3, 2], [4, 1, 9, 4])).toStrictEqual(true);
+    expect(sameOn([1, 2, 3, 3], [4, 1, 9, 4])).toStrictEqual(false);
+});
 test('sameOn2 test', () => {
-    expect(sameOn2([1,2,3,2], [4,1,9,4])).toStrictEqual(true);
-    expect(sameOn2([1,2,3,3], [4,1,9,4])).toStrictEqual(false);
-})
+    expect(sameOn2([1, 2, 3, 2], [4, 1, 9, 4])).toStrictEqual(true);
+    expect(sameOn2([1, 2, 3, 3], [4, 1, 9, 4])).toStrictEqual(false);
+});
 test('anagram test', () => {
     expect(anagram('anagram', 'nagaram')).toStrictEqual(true);
     expect(anagram('anagram', 'nagaraa')).toStrictEqual(false);
@@ -17,7 +22,7 @@ test('anagram test', () => {
     expect(anagram('anagram', 'nagaraa')).toStrictEqual(false);
     expect(anagram('anagram', 'nagara')).toStrictEqual(false);
     expect(anagram('abccc', 'abcd')).toStrictEqual(false);
-})
+});
 
 test('anagramSingleCounter test', () => {
     expect(anagramSingleCounter('anagram', 'nagaram')).toStrictEqual(true);
@@ -27,5 +32,4 @@ test('anagramSingleCounter test', () => {
     expect(anagramSingleCounter('anagram', 'nagaraa')).toStrictEqual(false);
     expect(anagramSingleCounter('anagram', 'nagara')).toStrictEqual(false);
     expect(anagramSingleCounter('abcc', 'abcd')).toStrictEqual(false);
-
-})
+});
