e1ea7c69fa3acc77ebe256b58446b883c39ceb5b
Malte Wirz <mail@maltewirz.com>
2020-09-01 15:07:12 +0200

    selectionSort

--- src/solving-patterns/sort-algorithms/sort-algorithms.spec.ts
+++ src/solving-patterns/sort-algorithms/sort-algorithms.spec.ts
@@ -1,14 +1,28 @@
-import {swap, swapNew, bubbleSort} from './sort-algorithms';
+import { swap, swapNew, bubbleSort, selectionSort } from './sort-algorithms';
 
 test('swap test', () => {
-    expect(swap([3,8,16,20], 1,2)).toStrictEqual([3,16,8,20]);
-})
+    expect(swap([3, 8, 16, 20], 1, 2)).toStrictEqual([3, 16, 8, 20]);
+});
 
 test('swapNew test', () => {
-    expect(swapNew([3,8,16,20], 1,2)).toStrictEqual([3,16,8,20]);
-})
+    expect(swapNew([3, 8, 16, 20], 1, 2)).toStrictEqual([3, 16, 8, 20]);
+});
 
 test('bubbleSort test', () => {
-    expect(bubbleSort([5,3,4,1,2])).toStrictEqual([1,2,3,4,5]);
-    expect(bubbleSort([8,1,2,3,4,5,6,7])).toStrictEqual([1,2,3,4,5,6,7,8]);
-})
+    expect(bubbleSort([5, 3, 4, 1, 2])).toStrictEqual([1, 2, 3, 4, 5]);
+    expect(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])).toStrictEqual([
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+    ]);
+});
+
+test('selectionSort test', () => {
+    expect(selectionSort([5, 3, 4, 1, 2])).toStrictEqual([1, 2, 3, 4, 5]);
+    expect(selectionSort([3,2,1])).toStrictEqual([1, 2, 3]);
+});
