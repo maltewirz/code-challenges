11d6314fcb82bae3da5b0110708e4aa33814fd2d
Malte Wirz <mail@maltewirz.com>
2020-09-10 14:32:50 +0200

    completed singlyLinkedLists

--- src/solving-patterns-algorithms-data-structures/data-structures/singlyLinkedList.spec.ts
+++ src/solving-patterns-algorithms-data-structures/data-structures/singlyLinkedList.spec.ts
@@ -15,6 +15,7 @@ describe('SinglyLinkedList', () => {
         expect(list).toMatchObject(emptyList);
 
         list.push(1);
+
         const singleItemList = {
             head: { val: 1, next: null },
             tail: { val: 1, next: null },
@@ -23,7 +24,7 @@ describe('SinglyLinkedList', () => {
         expect(list).toMatchObject(singleItemList);
 
         list.push(2);
-        
+
         const doubleItemList = {
             head: { val: 1, next: { val: 2, next: null } },
             tail: { val: 2, next: null },
@@ -39,11 +40,96 @@ describe('SinglyLinkedList', () => {
         expect(list.length).toStrictEqual(2);
     });
 
-    test('remove a value from the list', () => {
+    test('remove (pop) a value from the back of the list', () => {
         const list = new SinglyLinkedList();
         list.push(1);
         list.push(2);
-        list.pop();
+        expect(list.pop()).toMatchObject({ val: 2, next: null });
         expect(list.length).toStrictEqual(1);
+    });
+
+    test('remove (shift) a value from the start of the list', () => {
+        const list = new SinglyLinkedList();
+        list.push(1);
+        list.push(2);
+
+        list.shift();
+
+        expect(list.length).toStrictEqual(1);
+        expect(list.head).toMatchObject({ val: 2, next: null });
+    });
+
+    test('add (unshift) a value to the start of the list', () => {
+        const list = new SinglyLinkedList();
+        list.push(1);
+        list.push(2);
+
+        list.unshift(3);
+
+        expect(list.length).toStrictEqual(3);
+        expect(list.head.val).toStrictEqual(3);
+    });
+
+    test('get a value from a given index', () => {
+        const list = new SinglyLinkedList();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+
+        expect(list.get(0).val).toStrictEqual(1);
+        expect(list.get(3).val).toStrictEqual(4);
+    });
+
+    test('set a value from a given index', () => {
+        const list = new SinglyLinkedList();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+
+        expect(list.set(0, 5)).toStrictEqual(true);
+        expect(list.get(0).val).toStrictEqual(5);
+        expect(list.set(7, 5)).toStrictEqual(false);
+    });
+
+    test('insert a value from a given index', () => {
+        const list = new SinglyLinkedList();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+
+        expect(list.insert(1, 9)).toStrictEqual(true);
+        expect(list.get(0).val).toStrictEqual(1);
+        expect(list.get(1).val).toStrictEqual(9);
+        expect(list.get(2).val).toStrictEqual(2);
+    });
+
+    test('remove a value from a given index', () => {
+        const list = new SinglyLinkedList();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+
+        expect(list.remove(1).val).toStrictEqual(2);
+        expect(list.get(1).val).toStrictEqual(3);
+    });
+
+    test('print all values from the list in an array', () => {
+        const list = new SinglyLinkedList();
+        list.push(1);
+        list.push(2);
+        expect(list.print()).toStrictEqual([1, 2]);
+    });
+
+    test('reverse the list', () => {
+        const list = new SinglyLinkedList();
+        list.push(1);
+        list.push(2);
+        list.reverse()
+        expect(list.get(0).val).toStrictEqual(2);
+        expect(list.get(1).val).toStrictEqual(1);
     });
 });
--- src/solving-patterns-algorithms-data-structures/data-structures/singlyLinkedList.ts
+++ src/solving-patterns-algorithms-data-structures/data-structures/singlyLinkedList.ts
@@ -1,8 +1,22 @@
-// Singly Linked Lists Example:
+// #### Singly Linked Lists:
+// * A data structure that contains a head, tail and length property.
+// * Linked Lists consists of nodes, and each node has a value and a pointer to another node or null.
+// * Each node is connected one-directionaly to the next node
 // HEAD     LENGTH:4       TAIL
 // 4   ->   6   ->  8   ->  2
 //    NEXT     NEXT    NEXT
 
+// #####  Lists: Walk the stairs to your position
+
+// * Do not have indexes!
+// * Connected via nodes with a next pointer
+// * Random access is not allowed
+
+// Arrays: Take the elevator to the position
+
+// * Indexed in order!
+// * Insertion and deletion can be expensive
+// * Can quickyl be accessed at a specific index
 
 export class Node {
     val: number;
@@ -27,8 +41,8 @@ export class SinglyLinkedList {
         const newNode = new Node(val);
         if (!this.head) {
             this.head = newNode;
-            this.tail = this.head;
-        } else if (this.tail) {            
+            this.tail = newNode;
+        } else if (this.tail) {
             this.tail.next = newNode;
             this.tail = newNode;
         }
@@ -42,7 +56,7 @@ export class SinglyLinkedList {
         }
 
         let current = this.head;
-        let newTail = current;
+        let newTail = this.head;
 
         while (current.next) {
             newTail = current;
@@ -70,5 +84,111 @@ export class SinglyLinkedList {
             this.tail = null;
         }
         return currentHead;
+    }
+
+    unshift(val: number): SinglyLinkedList {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(index: number): Node | null {
+        if (index < 0 || index >= this.length) {
+            return null;
+        }
+        let counter = 0;
+        let current = this.head;
+
+        while (counter !== index && current) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+
+    set(index: number, val: number): boolean {
+        const foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index: number, val: number): boolean {
+        if (index < 0 || index > this.length) {
+            return false;
+        }
+        if (index === this.length) {
+            return !!this.push(val);
+        }
+        if (index === 0) {
+            return !!this.unshift(val);
+        }
+
+        const newNode = new Node(val);
+        const prev = this.get(index - 1);
+        if (prev) {
+            const temp = prev.next;
+            prev.next = newNode;
+            newNode.next = temp;
+        }
+        this.length++;
+        return true;
+    }
+
+    remove(index: number): Node | undefined {
+        if (index < 0 || index >= this.length) {
+            return undefined;
+        }
+        if (index === 0) {
+            return this.shift();
+        }
+        if (index === this.length - 1) {
+            return this.pop();
+        }
+        const previousNode = this.get(index - 1);
+        let removed;
+        if (previousNode && previousNode.next) {
+            removed = previousNode.next;
+            previousNode.next = removed.next;
+        }
+        this.length--;
+        return removed;
+    }
+
+    print(): number[] {
+        const arr: number[] = [];
+        let current = this.head;
+        while (current) {
+            arr.push(current.val);
+            current = current.next;
+        }
+        return arr;
+    }
+
+    reverse(): SinglyLinkedList {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let next;
+        let prev = null;
+        
+        for (let i = 0; i < this.length; i++) {
+            if (node) {
+                next = node.next;
+                node.next = prev;
+                prev = node;
+                node = next;
+            }
+        }
+        return this;
     }
 }
