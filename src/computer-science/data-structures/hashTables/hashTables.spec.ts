dffaa99f93d72a3d9ce554342919be46a76cd3c1
Malte Wirz <mail@maltewirz.com>
2020-09-25 10:23:25 +0200

    hashTables complete

--- src/computer-science/data-structures/hashTables/hashTables.spec.ts
+++ src/computer-science/data-structures/hashTables/hashTables.spec.ts
@@ -1,7 +1,47 @@
-import { hash } from './hashTables';
+import { hash, hashImproved, HashTable } from './hashTables';
 
 describe('Hash Tables', () => {
     test('string only hash function', () => {
-        expect(hash('pink', 10)).toStrictEqual(0)
-    })
-})
+        expect(hash('pink', 10)).toStrictEqual(0);
+        expect(hash('cyan', 10)).toStrictEqual(3);
+    });
+
+    test('string only hashImproved function', () => {
+        expect(hashImproved('pink', 10)).toStrictEqual(0);
+        expect(hashImproved('cyan', 10)).toStrictEqual(3);
+    });
+
+    test('creates a HashTable class instance', () => {
+        const hash = new HashTable();
+        expect(hash instanceof HashTable).toStrictEqual(true);
+    });
+
+    test('sets a hash', () => {
+        const hash = new HashTable();
+        hash.set('pink', '#654651');
+        expect(hash.keyMap[5][0]).toStrictEqual(['pink', '#654651']);
+        hash.set('pink', '#888894');
+        expect(hash.keyMap[5][1]).toStrictEqual(['pink', '#888894']);
+    });
+
+    test('gets a hash', () => {
+        const hash = new HashTable();
+        hash.set('pink', '#654651');
+        hash.set('blue', '#988894');
+        expect(hash.get('blue')).toStrictEqual('#988894');
+    });
+
+    test('gets a all values', () => {
+        const hash = new HashTable();
+        hash.set('pink', '#654651');
+        hash.set('blue', '#988894');
+        expect(hash.values()).toStrictEqual(['#654651', '#988894']);
+    });
+
+    test('gets a all keys', () => {
+        const hash = new HashTable();
+        hash.set('pink', '#654651');
+        hash.set('blue', '#988894');
+        expect(hash.keys()).toStrictEqual(['pink', 'blue']);
+    });
+});
--- src/computer-science/data-structures/hashTables/hashTables.ts
+++ src/computer-science/data-structures/hashTables/hashTables.ts
@@ -24,14 +24,114 @@
 // 2. Doesn't cluster outputs at specific indices, but distributes uniformly
 // 3. Deterministic - Same input yields same output
 
-// Hash for string only
+// Big O of Hash Tables
+// Insert: O(1)
+// Deletion: O(1)
+// Access: O(1)
+
+// Hash first function: string only, not constant time, not random
 
 export function hash(key: string, arrayLen: number): number {
     let total = 0;
-    for (const char of key) {
+    for (let i = 0; i < key.length; i++) {
         // map "a" to 1, "b" to 2, "c" to 3, etc
+        const char = key[i];
         const value = char.charCodeAt(0) - 96;
         total = (total + value) % arrayLen;
     }
     return total;
+}
+
+export function hashImproved(key: string, arrayLen: number): number {
+    let total = 0;
+    const WEIRD_PRIME = 31; // prime number to reduce collisions
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+        // map "a" to 1, "b" to 2, "c" to 3, etc
+        const char = key[i];
+        const value = char.charCodeAt(0) - 96;
+        total = (total * WEIRD_PRIME + value) % arrayLen;
+    }
+    return total;
+}
+
+// The prime number in the hash is helpful in spreading out the keys more uniformly.
+// It's also helpful if the array that you're putting values into has a prime length.
+
+// Dealing with collisions
+// Even with a large array and a great hash function, collisions are inevitable.
+// Strategies for dealing with collisions: Seperate Chaining or Linear Probing.
+
+// Seperate Chaining
+// With seperate chaining, at each index in our array we store values using a more sophisticated
+// data structure (e.g. an array or a linked list).
+
+// Linear Probing
+// With linear probing, when we find a collision, we search through the array to find the next empty slot.
+// Unlike with separate chaining, this allows us to store a single key-value at each index.
+
+export class HashTable {
+    keyMap: any[] = [];
+    constructor(size = 53) {
+        this.keyMap = new Array(size);
+    }
+
+    _hash(key: string): number {
+        let total = 0;
+        const WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            const char = key[i];
+            const value = char.charCodeAt(0) - 96;
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+        }
+        return total;
+    }
+
+    set(key: string, val: string): void {
+        const index = this._hash(key);
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, val]);
+    }
+
+    get(key: string): string | undefined {
+        const index = this._hash(key);
+        let result = undefined;
+        if (this.keyMap[index]) {
+            this.keyMap[index].forEach((element: string[]) => {
+                if (element[0] === key) {
+                    result = element[1];
+                }
+            });
+        }
+        return result;
+    }
+
+    // Values
+    // Loops through the hash table aray and returns
+    // and array of values in the table.
+
+    values(): string[] {
+        const valuesArr: string[] = [];
+        this.keyMap.forEach((mainArray) => {
+            mainArray.forEach((element: string[]) => {
+                valuesArr.push(element[1]);
+            });
+        });
+        return valuesArr;
+    }
+
+    // Keys
+    // Loops through the hash tables aray and returns
+    // an array of keys in the table.
+
+    keys(): string[] {
+        const keysArr: string[] = [];
+        this.keyMap.forEach((mainArray) => {
+            mainArray.forEach((element: string[]) => {
+                keysArr.push(element[0]);
+            });
+        });
+        return keysArr;
+    }
 }
