fe50242ede6c2ece054c0c8e5a1f8eadec39209a
Malte Wirz <mail@maltewirz.com>
2020-10-09 08:18:04 +0200

    formatting

--- src/code-challenges/7KYU/highAndLow/highAndLow.py
+++ src/code-challenges/7KYU/highAndLow/highAndLow.py
@@ -1,23 +1,23 @@
 # https://www.codewars.com/kata/554b4ac871d6813a03000035/train/python
-# In this little assignment you are given a string of space separated numbers, 
+# In this little assignment you are given a string of space separated numbers,
 # and have to return the highest and lowest number.
+
 
 def highAndLow(numbers):
     arr = numbers.split()
     lowest = int(arr[0])
     highest = int(arr[0])
-    if (len(arr) == 1):
-        return f'{arr[0]} {arr[0]}'
+    if len(arr) == 1:
+        return f"{arr[0]} {arr[0]}"
     if (len(arr) == 2) and arr[0] == arr[1]:
-        return f'{arr[0]} {arr[0]}'
+        return f"{arr[0]} {arr[0]}"
     for e in arr:
         number = int(e)
-        if (number < lowest):
+        if number < lowest:
             lowest = number
-        if (number > highest):
+        if number > highest:
             highest = number
-    return f'{highest} {lowest}'
-
+    return f"{highest} {lowest}"
 
 
 # Alternative:
