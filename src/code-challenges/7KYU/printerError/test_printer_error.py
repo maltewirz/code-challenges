2379cf01d6b2ec0c0c5ce5258c4b81849f4b1bf9
Malte Wirz <mail@maltewirz.com>
2020-10-19 09:16:49 +0200

    1 Kata 7KYU Python

---
+++ src/code-challenges/7KYU/printerError/test_printer_error.py
@@ -0,0 +1,14 @@
+from printer_error import printer_error
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_1(self):
+        result = printer_error("aaaxbbbbyyhwawiwjjjwwm")
+        self.assertEqual(result, "8/22")
+
+    def test_2(self):
+        result = printer_error("aaabbbbhaijjjm")
+        self.assertEqual(result, "0/14")
+if __name__ == "__main__":
+    unittest.main()
