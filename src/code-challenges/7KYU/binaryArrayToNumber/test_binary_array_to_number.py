02ff90ca33c879a1ad7e1751f3f81a608ebafe19
Malte Wirz <mail@maltewirz.com>
2020-10-22 08:07:27 +0200

    1Kata 7KYU Python

--- src/code-challenges/7KYU/binaryArrayToNumber/test_binary_array_to_number.py
+++ src/code-challenges/7KYU/binaryArrayToNumber/test_binary_array_to_number.py
@@ -7,9 +7,9 @@ class Test(unittest.TestCase):
         result = binary_array_to_number([0, 0, 0, 1])
         self.assertEqual(result, 1)
 
-    # def test_2(self):
-    #     result = binary_array_to_number([0, 0, 1, 0])
-    #     self.assertEqual(result, 2)
+    def test_2(self):
+        result = binary_array_to_number([0, 0, 1, 0])
+        self.assertEqual(result, 2)
 
 
 if __name__ == "__main__":
