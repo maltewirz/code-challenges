7a781b86f332b268bd5611eaa09a916d31a6c597
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2020-10-23 07:46:40 +0200

    1 Kata 7KYU Python

--- src/code-challenges/7KYU/divisors/divisors.ts
+++ src/code-challenges/7KYU/divisors/divisors.ts
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/542c0f198e077084c0000c2e/
 
-export function divisors(n: number) {
-    let results: number[] = [];
+export function divisors(n: number): number {
+    const results: number[] = [];
     for (let i = 1; i <= n; i++) {
         if (n % i === 0) {
             results.push(i);
