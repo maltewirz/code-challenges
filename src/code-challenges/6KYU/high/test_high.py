808d98da758f3f356437176739a86c90ff587781
Malte Wirz <mail@maltewirz.com>
2020-12-01 08:23:59 +0100

    1 Kata 5KYU Python

--- src/code-challenges/6KYU/high/test_high.py
+++ src/code-challenges/6KYU/high/test_high.py
@@ -4,13 +4,12 @@ import unittest
 
 class Test(unittest.TestCase):
     def test_1(self):
-        result = high('man i need a taxi up to ubud')
-        self.assertEqual(result, 'taxi')
+        result = high("man i need a taxi up to ubud")
+        self.assertEqual(result, "taxi")
 
     def test_2(self):
-        result = high('what time are we climbing up the volcano')
-        self.assertEqual(result, 'volcano')
-
+        result = high("what time are we climbing up the volcano")
+        self.assertEqual(result, "volcano")
 
 
 if __name__ == "__main__":
