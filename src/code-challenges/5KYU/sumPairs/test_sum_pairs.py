6801846b2c18eb6c7798a7241d967a197c0ad278
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2020-12-08 08:30:01 +0100

    1 Kata 5KYU Python

--- src/code-challenges/5KYU/sumPairs/test_sum_pairs.py
+++ src/code-challenges/5KYU/sumPairs/test_sum_pairs.py
@@ -5,7 +5,7 @@ import unittest
 class Test(unittest.TestCase):
     def test_1(self):
         result = sum_pairs([4, 3, 2, 3, 4], 6)
-        self.assertEqual(result,  [4, 2])
+        self.assertEqual(result, [4, 2])
 
     def test_2(self):
         result = sum_pairs([10, 5, 2, 3, 7, 5], 10)
