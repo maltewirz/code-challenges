9ff5fea979e3276399a7c761a92f48d570940fe4
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2021-04-13 06:14:54 +0000

    rotateImage

---
+++ src/leetcode/array/rotateImage.spec.ts
@@ -0,0 +1,6 @@
+import { rotateImage } from './rotateImage';
+
+test('Basic Test', () => {
+    expect(rotateImage([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+        .toStrictEqual([[7, 4, 1], [8, 5, 2], [9, 6, 3]])
+})
