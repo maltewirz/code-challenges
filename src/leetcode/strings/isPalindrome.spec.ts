11baa186dd2c841a5d0f1748c571febbef8a138c
Malte Wirz <mail@maltewirz.com>
2021-04-20 07:48:16 +0000

    isPalindrome

---
+++ src/leetcode/strings/isPalindrome.spec.ts
@@ -0,0 +1,8 @@
+import { isPalindrome } from './isPalindrome';
+
+test('Basic Test', () => {
+    expect(isPalindrome("A man, a plan, a canal: Panama")).toStrictEqual(true)
+    expect(isPalindrome("race a car")).toStrictEqual(false)
+    expect(isPalindrome("0P")).toStrictEqual(false)
+    
+})
