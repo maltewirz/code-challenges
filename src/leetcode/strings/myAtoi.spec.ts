7780228489489dc582da8bcae2ef997547ac349a
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2021-04-21 07:28:31 +0000

    myAtoi

---
+++ src/leetcode/strings/myAtoi.spec.ts
@@ -0,0 +1,11 @@
+import { myAtoi } from './myAtoi';
+
+test('Basic Test', () => {
+    expect(myAtoi("42")).toStrictEqual(42);
+    expect(myAtoi("   -42")).toStrictEqual(-42);
+    expect(myAtoi("4193 with words")).toStrictEqual(4193);
+    expect(myAtoi("words and 987")).toStrictEqual(0);
+    expect(myAtoi("-91283472332")).toStrictEqual(-2147483648);
+    expect(myAtoi("+-12")).toStrictEqual(0);
+    
+})
---
+++ src/leetcode/strings/myAtoi.ts
@@ -0,0 +1,25 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/884/
+// Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+export function myAtoi(s: string): number {
+    const trimmedS = s.trim();
+    if (trimmedS === '') {
+        return 0;
+    }
+
+    const parsedInt = Number.parseInt(trimmedS, 10);
+    if (Number.isNaN(parsedInt)) {
+        return 0;
+    }
+
+    const minRange = Math.pow(-2, 31);
+    if (parsedInt < minRange) {
+        return minRange;
+    }
+    const maxRange = Math.pow(2, 31)-1;
+    if (parsedInt > maxRange) {
+        return maxRange;
+    }
+    return parsedInt;
+}
+
