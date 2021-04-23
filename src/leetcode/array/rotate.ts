c4b5bf9384539ebe6c995b27b7a2c1de15ea4a60
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2021-04-23 06:05:00 +0000

    typos and comments

--- src/leetcode/array/rotate.ts
+++ src/leetcode/array/rotate.ts
@@ -2,17 +2,17 @@
 Given an array, rotate the array to the right by k steps, where k is non-negative. 
 Do not return anything, modify nums in-place instead.
  */
-export function rotate1(nums: number[], k: number): number[] {
-    if (nums.length > 1)  {
+export function rotate1(nums: number[], k: number): number[] | undefined {
+    if (nums.length > 1) {
         for (let i = 0; i < k; i++) {
-            const last= nums.pop()!;
+            const last = nums.pop()!;
             nums.unshift(last);
         }
         return nums;
     }
 };
 
-export function rotate2(nums: number[], k: number): number[] {
+export function rotate2(nums: number[], k: number): number[] | undefined {
     nums.unshift(...nums.splice(nums.length - k))
     return nums
 };
