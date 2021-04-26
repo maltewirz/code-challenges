4f2796f2dfedde4b41eb0b2b524709b0d467d382
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2021-04-26 07:34:22 +0000

    formatting with prettier enforced

--- src/leetcode/array/maxProfit.spec.ts
+++ src/leetcode/array/maxProfit.spec.ts
@@ -7,5 +7,5 @@ test('Basic Test maxProfit', () => {
 
 test('Basic Test maxProfitBest', () => {
     expect(maxProfitBest([1, 2, 3, 4, 5])).toStrictEqual(4);
-    expect(maxProfitBest([7, 1, 5, 3, 6, 4])).toStrictEqual(7)
+    expect(maxProfitBest([7, 1, 5, 3, 6, 4])).toStrictEqual(7);
 });
