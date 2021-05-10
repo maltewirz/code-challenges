573cadac58fa0e9f1ba188434d215a4db795dbb2
Malte Wirz <mail@maltewirz.com>
2021-05-10 09:06:54 +0200

    isSymetric

--- src/computer-science/data-structures/trees/isValidBST.ts
+++ src/computer-science/data-structures/trees/isValidBST.ts
@@ -13,6 +13,14 @@
 // Input: root = [5,1,4,null,null,3,6]
 // Output: false
 // Explanation: The root node's value is 5 but its right child's value is 4.
+//     5
+// 1       4
+//       3   6
+
+// Another false BST:
+//     5
+// 4       6
+//       3   7
 
 class TreeNode {
     val: number;
@@ -30,34 +38,46 @@ function isValidBST(root: TreeNode | null): boolean | number {
         return true; // Sanity check for passing test case '[]'
     }
 
-    function helper(root: TreeNode | null, min: number | null, max: number | null): any {
+    function helper(
+        root: TreeNode | null,
+        min: number | null,
+        max: number | null
+    ): any {
         if (!root) {
             return true; // We hit the end of the path
         }
-        
-        if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) {
+
+        if (
+            (min !== null && root.val <= min) ||
+            (max !== null && root.val >= max)
+        ) {
             return false; // current node's val doesn't satisfy the BST rules
         }
-        
+
         // Continue to scan left and right
-        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+        return (
+            helper(root.left, min, root.val) &&
+            helper(root.right, root.val, max)
+        );
     }
-    
+
     return helper(root, null, null);
 }
 
+function isValidBSTRecursive(
+    root: TreeNode | null,
+    min: number | TreeNode | null = null,
+    max: number | TreeNode | null = null
+): boolean | number {
+    if (!root) return true;
+    if (min && root.val <= min.val) return false;
+    if (max && root.val >= max.val) return false;
+    return (
+        isValidBSTRecursive(root.left, min, root) &&
+        isValidBSTRecursive(root.right, root, max)
+    );
+}
 
-// function isValidBST2(root: TreeNode, min=null, max=null): boolean | number {
-//     if (!root) return true;
-//     if (min && root.val <= min.val) return false;
-//     if (max && root.val >= max.val) return false;
-//     return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
-// }
-
-// get currentNode, is currentNode.left smaller and currentNode.right greater? - if not, return false
-//
-
-
-            //     5
-            // 4       6
-            //       3   7
+// Complexity
+// Time: O(n)
+// Space: O(n)
