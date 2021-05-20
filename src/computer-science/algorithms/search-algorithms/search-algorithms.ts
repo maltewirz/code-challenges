ed4b64e7b60cd3297c2ffddeff58aba4d9da13b8
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2021-05-20 07:25:37 +0200

    isBadVersion more comments for example walkthrough

--- src/computer-science/algorithms/search-algorithms/search-algorithms.ts
+++ src/computer-science/algorithms/search-algorithms/search-algorithms.ts
@@ -32,7 +32,7 @@ export function linearSearch(arr: string[], val: string): number {
 export function binarySearch(arr: number[], val: number): number {
     let left = 0;
     let right = arr.length - 1;
-    let middle = Math.floor((right + left) / 2);
+    let middle = Math.floor((left + right) / 2);
     while (arr[middle] !== val && left <= right) {
         if (arr[middle] < val) {
             // we add one to avoid redundant code - we already know its not the middle
@@ -67,10 +67,7 @@ export function naiveStringSearch(str: string, search: string): number {
     return count;
 }
 
-export function naiveStringSearchRefactor(
-    str: string,
-    search: string
-): number {
+export function naiveStringSearchRefactor(str: string, search: string): number {
     let count = 0;
     for (let i = 0; i < str.length; i++) {
         for (let j = 0; j < search.length; j++) {
