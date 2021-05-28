43912c354daea97ae8711ac1090d1d506abed8f6
Malte Wirz <mail@maltewirz.com>
2021-05-28 08:41:56 +0200

    rob further versions

--- src/leetcode/dynamicProgramming/rob.spec.ts
+++ src/leetcode/dynamicProgramming/rob.spec.ts
@@ -1,5 +1,7 @@
-import { rob } from './rob';
+import { robRecursive, robIterative } from './rob';
 
 test('Basic test', () => {
-    expect(rob([1, 2, 3, 1])).toStrictEqual(4);
+    expect(robRecursive([1, 2, 3, 1])).toStrictEqual(4);
+    // expect(robIterative([1, 2, 3, 1])).toStrictEqual(4);
+    expect(robIterative([2, 7, 9, 3, 1])).toStrictEqual(12);
 });
