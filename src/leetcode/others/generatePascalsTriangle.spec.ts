6fcd6325283b4614d9d14869d1a797dbecfb67d3
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2021-07-23 08:54:48 +0200

    generatePascalsTriangle

---
+++ src/leetcode/others/generatePascalsTriangle.spec.ts
@@ -0,0 +1,17 @@
+import { generatePascalsTriangle } from './generatePascalsTriangle';
+
+test('Basic Test', () => {
+    expect(generatePascalsTriangle(5)).toStrictEqual([
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+        [1, 4, 6, 4, 1],
+    ]);
+    expect(generatePascalsTriangle(3)).toStrictEqual([
+        [1],
+        [1, 1],
+        [1, 2, 1],
+
+    ]);
+});
