5ec8fc64b6cd5202abf9bc3c56bd125f4ba67b8a
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2021-07-31 12:42:44 +0200

    twoSum with dictionary

--- src/leetcode/array/twoSum.spec.ts
+++ src/leetcode/array/twoSum.spec.ts
@@ -1,7 +1,9 @@
-import { twoSum } from './twoSum';
+import { twoSum, twoSum2 } from './twoSum';
 
 test('Basic Test', () => {
     expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
     expect(twoSum([3, 2, 4], 6)).toStrictEqual([1, 2]);
     expect(twoSum([3, 3], 6)).toStrictEqual([0, 1]);
+
+    expect(twoSum2([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
 });
--- src/leetcode/array/twoSum.ts
+++ src/leetcode/array/twoSum.ts
@@ -6,6 +6,20 @@
 // use the same element twice.
 // You can return the answer in any order.
 
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
 export function twoSum(nums: number[], target: number): number[] | undefined {
     for (let i = 0; i < nums.length - 1; i++) {
         for (let j = i + 1; j < nums.length; j++) {
@@ -23,3 +37,27 @@ export function twoSum(nums: number[], target: number): number[] | undefined {
 // through the rest of array which takes O(n) time. Therefore, the time complexity is O(n^2).
 
 // Space complexity : O(1).
+
+// Alternative:
+export function twoSum2(nums: number[], target: number): number[] | undefined {
+    const dict: { [key: number]: number } = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const curr = nums[i];
+        const complement = target - curr;
+
+        if (complement in dict) {
+            return [dict[complement], i];
+        }
+        dict[curr] = i;
+    }
+    return undefined;
+}
+
+/**
+ * loop through nums
+ * save in dict: the value and its index
+ *
+ * for each number:
+ *      is the target minus the current number in the dict ?
+ */
