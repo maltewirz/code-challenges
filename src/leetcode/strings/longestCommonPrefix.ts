df6b212139b4dc8f11ff5e57ce316593768e56ae
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2021-08-04 15:03:34 +0200

    updated some descriptions

--- src/leetcode/strings/longestCommonPrefix.ts
+++ src/leetcode/strings/longestCommonPrefix.ts
@@ -3,6 +3,17 @@
 
 // If there is no common prefix, return an empty string "".
 
+
+// Example 1:
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+// Example 2:
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+ 
+
 export function longestCommonPrefix(strs: string[]): string {
     if (strs.length === 0) {
         return '';
